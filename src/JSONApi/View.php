50659d65232915f8426fe273da00e333c9ad4205
Andrzej Kostrzewa <andrewprofile@users.noreply.github.com>
2015-10-17 21:39:15 +0200

    Update View.php

--- src/JSONApi/View.php
+++ src/JSONApi/View.php
@@ -9,29 +9,29 @@ use Slim\Slim;
 */
 class View extends \Slim\View
 {
-    private $app = null;
+	private $app = null;
 
-    private $encodingJSON = 0;
+	private $encodingJSON = 0;
 
-    private static $contentType = [
-        'json'  => 'application/json',
-        'jsonp' => 'application/javascript'
-    ];
+	private static $contentType = [
+		'json'  => 'application/json',
+		'jsonp' => 'application/javascript'
+	];
 
-    public function __construct(Slim $app = null)
+	public function __construct(Slim $app = null)
     {
-        $this->app = ($app instanceof Slim) ? $app : Slim::getInstance();
+		$this->app = ($app instanceof Slim) ? $app : Slim::getInstance();
         parent::__construct();
-    }
+	}
 
-    public function render($status = 200, $data = null)
-    {
+	public function render($status = 200, $data = null)
+	{
 
         $app = $this->app;
 
-        $status = (int) $status;
+		$status = (int) $status;
 
-        $JSONResponse = $this->all();
+		$JSONResponse = $this->all();
 
         if (!$this->has('error')) {
             $JSONResponse['error'] = false;
@@ -39,7 +39,7 @@ class View extends \Slim\View
 
         $JSONResponse['status'] = $status;
 
-        if(isset($this->data->flash) && is_object($this->data->flash)){
+		if(isset($this->data->flash) && is_object($this->data->flash)){
             $flashMessages = $this->data->flash->getMessages();
             if (count($flashMessages)) {
                 $JSONResponse['flash'] = $flashMessages;
@@ -50,19 +50,20 @@ class View extends \Slim\View
 
         $JSONResponse = isset($JSONResponse[0]) && count($JSONResponse) === 1 && is_scalar($JSONResponse[0]) ? $JSONResponse[0] : $JSONResponse;
 
+
         $app->response()->header('Content-Type', View::$contentType['json'].'; charset=utf-8');
 
         $jsonp_callback = $app->request->get('callback', null);
 
         if ($jsonp_callback !== null) {
-            $app->response()->header('Content-Type', View::$contentType['jsonp'].'; charset=utf-8');
+        	$app->response()->header('Content-Type', View::$contentType['jsonp'].'; charset=utf-8');
             $app->response()->body(call_user_func_array($jsonp_callback, [json_encode($JSONResponse, $this->encodingJSON)]));
         } else {
             $app->response()->body(json_encode($JSONResponse, $this->encodingJSON | JSON_UNESCAPED_UNICODE));
         }
 
         $app->stop();
-    }
+	}
 
     public function setEncodingJSON($encodingCode)
     {
